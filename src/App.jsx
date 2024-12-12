5b6b22367349da7e2659a9b28ca8bdfcfc12636c
MH Code <mazheriftikhar1@gmail.com>
2024-12-12 19:04:11 +0500

    12-Dec changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import Card1 from './Components/Card1/Card1'
+import Card2 from './Components/Card2/Card2'
+import Card3 from './Components/Card3/Card3'
+import Card4 from './Components/Card4/Card4'
+import Card5 from './Components/Card5/Card5'
+import Card6 from './Components/Card6/Card6'
+import Card7 from './Components/Card7/Card7'
+import Card8 from './Components/Card8/Card8'
+import Card9 from './Components/Card9/Card9'
+import Card10 from './Components/Card10/Card10'
+import Card11 from './Components/Card11/Card11'
+import Card12 from './Components/Card12/Card12'
+import Card13 from './Components/Card13/Card13'
+import Card14 from './Components/Card14/Card14'
+import Card15 from './Components/Card15/Card15'
 
 const App = () => {
   return (
     <div>
       <Card1 />
+      <Card3 />
+      <Card2 />
+      <Card4 />
+      <Card5 />
+      <Card6 />
+      <Card7 />
+      <Card8 />
+      <Card9 />
+      <Card10 />
+      <Card11 />
+      <Card12 />
+      <Card13 />
+      <Card14 />
+      <Card15 />
     </div>
   )
 }
